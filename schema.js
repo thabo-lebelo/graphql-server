917b25d70209ba27a3f6fd6fa271c0168441957c
Thabo Lebelo <thabolebelo@icloud.com>
2022-01-15 16:27:00 +0200

    Express GraphQL Setup

---
+++ schema.js
@@ -0,0 +1,12 @@
+const { 
+    GraphQLObjectType,
+    GraphQLShema,
+    GraphQLString,
+    GraphQLList,
+    GraphQLInt,
+    GraphQLNonNull
+} = require('graphql')
+
+module.exports = new GraphQLShema({
+    
+})
