1cfa1ce38cd61b320c39de46d46a3439f26ce840
Thabo Lebelo <thabolebelo@icloud.com>
2022-01-15 17:30:20 +0200

    JSON Server & Axios

--- schema.js
+++ schema.js
@@ -7,12 +7,16 @@ const {
     GraphQLNonNull
 } = require('graphql')
 
+const axios = require('axios')
+
 //Dummy data
+/*
 const customers = [
     { id: '1', name: 'John Doe', email: 'john@gmail.com', age: 35 },
     { id: '2', name: 'Steve Smith', email: 'steve@gmail.com', age: 25 },
     { id: '3', name: 'Sara Williams', email: 'sara@gmail.com', age: 32 }
 ];
+*/
 
 // CustomerType
 const CustomerType = new GraphQLObjectType({
@@ -35,17 +39,22 @@ const RootQuery = new GraphQLObjectType({
                 id: { type: GraphQLString }
             },
             resolve(parentValue, args) {
+                /*
                 for (let i = 0; i < customers.length; i++) {
                     if (customers[i].id == args.id) {
                         return customers[i]
                     }
                 }
+                */
+               return axios.get(`http://localhost:3000/customers/${args.id}`)
+                .then((response) => response.data)
             }
         },
         customers: {
             type: new GraphQLList(CustomerType),
             resolve(parentValue, args) {
-                return customers
+                return axios.get(`http://localhost:3000/customers`)
+                .then(response => response.data)
             }
         }
     }
